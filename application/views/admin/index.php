1408f5f304de1b9be4ad22ee77de48059e0e533c
wihanuralim <wihanuralim74@gmail.com>
2020-04-18 00:10:46 +0700

    add dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,7 +2,66 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+    <div class="card text-white" style="max-width: 1080px; ">
+        <div class="card-header bg-primary">
+            <h5 class="card-title"><b>SEKILAS HIMPANA (PERHIMPUNAN PENSIUNAN PERTAMINA)</b></h5>
+        </div>
+    </div>
+    <div class="card text-black bg-white mb-3" style="max-width: 1080px;">
+        <div class="row no-gutters">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="<?= base_url('assets/img/utl/LOGO-HIMPANA.png'); ?>" class="card-img" weight="100px" style="padding : 10px; margin-left: 30px; width: 125px; height: 125px;" alt="...">
+                </div>
+                <div class="col-md-10">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align :justify">HIMPANA merupakan wadah para pensiunan Pertamina
+                            yang didirikan pada tanggal 2 November 1985 melalui akte notaris no.73, Ny.Anna Sunarhadi SH dengan nama Perhimpunan Purna Karyawan Pertamina (HIMPANA) untuk waktu yang tidak ditentukan dan berkedudukan di Jakarta. Sejak November 2002, melalui Musyawarah Nasional, nama organisasi diubah menjadi Perhimpunan Pesiunan Pertamina (HIMPANA). HIMPANA di dirikan bertujuan untuk :</p>
+                    </div>
+                </div>
+                <div class="row col-md-12">
+                    <ol style="text-align: justify">
+                        <li>Menjadi wadah komunikasi sosial dan membina serta menyelenggarakan hubungan baik antar sesama anggota, memupuk kekeluargaan, persaudaraan dan kesetiakawanan.</li>
+                        <li>Mendayagunakan pengetahuan dan pengalaman para anggota dalam berbagai usaha untuk meningkatkan kesejahteraan kehidupan lahiriah dan batiniah.</li>
+                        <li>Menampung dan mengembangkan usaha dan kerjasama, baik antar anggota maupun dengan badan-badan dan instansi lain</li>
+                        <li>Ikut serta berpartisipasi dalam membangun bangsa dan negara</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-deck mb-4">
+        <div class="card">
+            <div class="card-header text-white bg-primary">
+                <h5 style="text-align: center"><b>VISI HIMPANA</b></h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text mt-4" style="font-size: 24px; text-align:center">"Memupuk tali silaturahmi serta menggerakan kegiatan-kegiatan yang sehat dan bermanfaat"</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header text-white bg-primary">
+                <h5 style="text-align: center"><b>MISI HIMPANA</b></h5>
+            </div>
+            <div class="card-body">
+                <ol style="text-align: left">
+                    <li>
+                        Menyelenggarakan komunikasi sosial untuk melestarikan hubungan kekeluargaan, persaudaraan dan kesetiakawanan
+                    </li>
+                    <li>
+                        Mendayagunakan potensi pengetahuan dan pengalaman dalam berbagai kegiatan dan usaha
+                    </li>
+                    <li>
+                        Berupaya mensejahterakan kehidupan jasmani dan bathiniah
+                    </li>
+                    <li>
+                        Berpartisipasi, khususnya di bidang migas, dalam pembangunan bangsa dan negara
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+    </div>
 
 
 
